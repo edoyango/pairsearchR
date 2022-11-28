dd4f048124bb34722aa081200f6e0ea59a797e82
Edward Yang <yang.e@wehi.edu.au>
2022-11-28 16:52:53 +1100

    first funtion definition

---
+++ R/dsearch_pairsR.R
@@ -0,0 +1,14 @@
+dsearch_pairsR <- function(pts, r) {
+  npts = length(pts)
+  nprs = 0
+  prs = list()
+  for (i in 1:(npts-1)) {
+    for (j in (i+1):npts) {
+      if (norm(pts[[i]]-pts[[j]], type="2") <= r) {
+        nprs <- nprs + 1
+        prs[[nprs]] <- c(i, j)
+      }
+    }
+  }
+  return(prs)
+}
